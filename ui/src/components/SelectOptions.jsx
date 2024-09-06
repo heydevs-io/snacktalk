cc54d2efd16054f19b9044d353de835cab38dc24
Truong Quoc Hoi <hoitrnd107@gmail.com>
2024-09-06 09:42:43 +0700

    chore: update form sign up account

---
+++ ui/src/components/SelectOptions.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Dropdown from './Dropdown';
+
+const SelectOptions = ({ name, options, value, onChange }) => {
+  const text = value && options.filter((opt) => opt.id === value)[0].text;
+
+  const handleClick = (value) => {
+    if (onChange) {
+      onChange(value);
+    }
+  };
+
+  return (
+    <Dropdown
+      target={
+        <button type="button" className="select-bar-dp-target">
+          {text ?? 'Select'}
+        </button>
+      }
+    >
+      <div className="dropdown-list">
+        {options.map((option) => (
+          <div className="dropdown-item" key={option.id} onClick={() => handleClick(option.id)}>
+            {option.text}
+          </div>
+        ))}
+      </div>
+    </Dropdown>
+  );
+};
+
+SelectOptions.propTypes = {
+  name: PropTypes.string,
+  onChange: PropTypes.func,
+  options: PropTypes.array.isRequired,
+};
+
+export default SelectOptions;
